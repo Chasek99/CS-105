203780d9cbac5a0d5bbede77455f1c2ed3083293
Chase <ck121642@students.davenport.k12.ia.us>
2017-08-28 20:49:34 -0500

    Add files via upload
    First CS105 Assignment

---
+++ src/firstpackage/Hello.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package firstpackage;
+
+/**
+ * @author Kress
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.print(" Hello World\n");
+		System.out.print(" My name is Chase Kress\n\n");//code that displays my name on console
+		
+		System.out.print("/ \\ // \\\\ /// \\\\\\\n\n");
+		
+		System.out.print("This program prints a\n"
+				+ "quote from the Gettysburg Address.\n\n"
+				
+				+"Four score and seven years ago,\n"
+				+"our 'four fathers' brought forth on\n"
+				+"this continent a new nation.");
+		
+		System.out.print("A \"quoted\" String is");
+		System.out.print("");
+		System.out.print("");
+		System.out.print("");
+		
+		
+	
+	}
+
+}
